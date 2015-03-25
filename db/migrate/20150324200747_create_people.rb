18e3a29c2c2747217a152f578357355c7d750b6d
twotimes22 <thomas.weld@gmail.com>
2015-03-25 17:22:31 -0400

    initial commit

---
+++ db/migrate/20150324200747_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.integer :height
+      t.integer :weight
+      t.string :gender
+
+      t.timestamps null: false
+    end
+  end
+end
